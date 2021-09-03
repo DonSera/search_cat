124be8f6ccb0c6d75504810a5c9893c2b8a4a379
DonSera <sera.2021@lifeoasis.com>
2021-09-03 14:28:00 +0900

    fetch 앞뒤로 Loading 띄우고 삭제하기

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
 let catDiv = document.getElementById('catDiv');
 let inputType = document.getElementById('inputType');
+let loadingDiv = document.getElementById('loadDiv');
 
 async function setType() {
+    loadingDiv.innerHTML = 'Loading'; // 로딩 띄우기
     const imgUrlArray = []; //겹치는 url 확인용
     const type = inputType.value;
     const searchUrl = "https://oivhcpn8r9.execute-api.ap-northeast-2.amazonaws.com/dev/api/cats/search?q=" + type;
     console.log(type);
 
     try {
+        console.log('start fetch');
         const res = await fetch(searchUrl);
         const json = await res.json();
+        console.log('end fetch');
+        loadingDiv.innerHTML = ''; // 로딩 삭제
         catDiv.innerHTML = ''; // 이미지 화면에서 지우기
 
         if (json.message) {
             // 결과를 받지 못한 경우
-            catDiv.append('None');
+            catDiv.append('Message None');
         } else {
             const dataArray = json.data;
             const arrayLen = Object.keys(dataArray).length;
 
             if (arrayLen === 0) {
                 // 입력된 글자가 없는 경우
-                catDiv.append('None');
+                catDiv.append('Get None');
             } else {
                 const standLoop = 15; //한번에 로딩할 크기
                 if (arrayLen > standLoop) {
